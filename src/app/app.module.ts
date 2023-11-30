fdb06773aefc5a0f30d4112ab290a19fac52d751
addinkamdv <addin_cakep@ymail.com>
2023-12-01 01:28:51 +0700

    add slider

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,24 +1,34 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
+import { FormsModule } from '@angular/forms';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { ProductListComponent } from './product-list/product-list.component';
-
 import { HttpClientModule } from '@angular/common/http';
 import { ProductDetailComponent } from './product-detail/product-detail.component';
+import { CarouselModule } from 'ngx-owl-carousel-o';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { NgbModule } from "@ng-bootstrap/ng-bootstrap";
+
 @NgModule({
   declarations: [
     AppComponent,
     ProductListComponent,
-    ProductDetailComponent
+    ProductDetailComponent,
+   
   ],
   imports: [
     HttpClientModule,
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    CarouselModule,
+    NgbModule,
+    FormsModule
+    
   ],
   providers: [],
   bootstrap: [AppComponent]
+  
 })
 export class AppModule { }
--- src/app/product-detail/product-detail.component.ts
+++ src/app/product-detail/product-detail.component.ts
@@ -1,21 +1,38 @@
 import { Component } from '@angular/core';
 import { ActivatedRoute, Router } from '@angular/router';
 import { ProductService } from '../services/product.service';
+import { HttpClient } from '@angular/common/http';
+import { NgbCarouselConfig } from '@ng-bootstrap/ng-bootstrap';
+
+
 @Component({
   selector: 'app-product-detail',
   templateUrl: './product-detail.component.html',
-  styleUrl: './product-detail.component.css'
+  styleUrl: './product-detail.component.css',
+ 
 })
-export class ProductDetailComponent {
+export class ProductDetailComponent    {
   details:any;
-  constructor(private productService: ProductService,private route: ActivatedRoute, public router: Router){
+
+
+
+  constructor(private productService: ProductService,private route: ActivatedRoute, public router: Router, private readonly http: HttpClient){
+  }
+  ngAfterViewInit(): void {
+    throw new Error('Method not implemented.');
   }
   ngOnInit(){
-    let index = this.route.snapshot.paramMap.get('id');
+    
+    let index = this.
+    route.snapshot.paramMap.get('id');
     console.log(index);
     this.productService.getProductDetail(index).subscribe(data => {
       console.log(data)
       this.details=data;
+       
+ 
     });
   }
+ 
+ 
 }
